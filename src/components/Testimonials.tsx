bc2bf41753ea60d737d9298997a79994442fccd3
thekinita <thekinita@mail.ru>
2025-03-13 10:23:38 +0500

    refactor: add drag for Testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion, useDragControls } from 'framer-motion'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 import { testimonials } from '../../public/testimonials'
 
 const Testimonials = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
   const [xSide, setXSide] = useState(0)
+  const dragControls = useDragControls()
 
   const prevTestimonial = () => {
     setCurrentIndex((prev) => (prev === 0 ? testimonials.length - 1 : prev - 1))
@@ -22,20 +23,27 @@ const Testimonials = () => {
   return (
     <section className='relative w-full mx-auto text-center'>
       <h2 className='text-3xl font-bold text-accent'>Отзывы наших клиентов</h2>
-      <div className='flex justify-between items-center py-20 px-6py-20 px-6'>
+      <div className='flex justify-between items-center py-20 px-6'>
         <button
           onClick={prevTestimonial}
           className='cursor-pointer p-2 hover:text-accent duration-200'
         >
           <ChevronLeftIcon className='w-6 h-6' />
         </button>
-        <div>
+        <div className='overflow-visible w-full max-w-2xl'>
           <motion.div
             key={testimonials[currentIndex].company}
             initial={{ opacity: 0, x: xSide }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: xSide }}
             transition={{ duration: 0.5 }}
+            drag='x'
+            dragConstraints={{ left: 0, right: 0 }}
+            dragElastic={0.2}
+            onDragEnd={(_, info) => {
+              if (info.offset.x > 200) prevTestimonial()
+              if (info.offset.x < -200) nextTestimonial()
+            }}
           >
             <h3 className='text-2xl font-semibold mb-4'>
               {testimonials[currentIndex].company}

52b2a2df85de6fdeec9fb9af1fe048a004690929
thekinita <thekinita@mail.ru>
2025-03-21 06:42:07 +0500

    fix: fix bg on date inputs

--- src/components/form-wrappers/DateInputForm.tsx
+++ src/components/form-wrappers/DateInputForm.tsx
@@ -18,11 +18,10 @@ export const DateInputForm = ({
   return (
     <div className="w-full p-2">
       <label className="text-sm font-medium">{label}</label>
-      <div className="mt-1 relative">
+      <div className="mt-1 relative w-full rounded-lg border-1 border-transparent dark:bg-white/5 bg-black/5 py-2 px-3 text-sm">
         <input
           type={type}
           className={clsx(
-            'w-full rounded-lg border-1 border-transparent dark:bg-white/5 bg-black/5 py-2 px-3 text-sm',
             'focus:outline-none focus:border-forlight dark:focus:border-fordark data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25',
             '[&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-inner-spin-button]:hidden [&::-webkit-clear-button]:hidden',
             className

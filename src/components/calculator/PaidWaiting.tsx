274fd25a8718559744cd1145f0773fab93798229
thekinita <thekinita@mail.ru>
2025-03-30 03:26:04 +0500

    fix: fix PaidWaiting component

--- src/components/calculator/PaidWaiting.tsx
+++ src/components/calculator/PaidWaiting.tsx
@@ -4,32 +4,35 @@ export const PaidWaiting = ({
   unloadAddress,
   cargoWeight,
   cargoType,
-  cargoCount
+  palletCount,
+  boxCount
 }: TransportParams) => {
   const cost =
-    cargoWeight < 1.5 && cargoCount <= 10
+    cargoWeight < 1.5 && palletCount <= 10
       ? 700
-      : cargoWeight >= 1.5 && cargoWeight < 3 && cargoCount <= 12
+      : cargoWeight >= 1.5 && cargoWeight < 3 && palletCount <= 12
       ? 800
       : cargoWeight >= 3 && 1000
 
   if (
     (cargoType === 'Паллет' &&
       cargoWeight <= 5 &&
-      cargoCount <= 15 &&
+      palletCount <= 15 &&
+      cost &&
+      boxCount === 0 &&
       unloadAddress === 'Доставка до СЦ и РЦ по Москве и МО') ||
     unloadAddress === 'Доставка до РЦ Алексин' ||
     unloadAddress === 'Доставка до РЦ Тверь'
   ) {
     return (
-      <span className='text-xs'>
+      <span className="text-xs">
         Ожидание на выгрузке 2 ч. бесплатно, далее {cost} руб./ч.
       </span>
     )
   }
 
   return (
-    <span className='text-xs'>
+    <span className="text-xs">
       Стоимость простоя на выгрузке обсужается индивидуально.
     </span>
   )
